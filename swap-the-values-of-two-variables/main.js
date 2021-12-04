c7d9f568fdad6f561b300337a1656feca384cff6
reda hammada <hammada.reda.dev@gmail.com>
2021-12-04 16:46:40 +0100

    swap the variable values exercise added

---
+++ swap-the-values-of-two-variables/main.js
@@ -0,0 +1,20 @@
+// variables declaration 
+var a;
+var b;
+var c;
+
+// input 
+
+a = 50;
+b = 100;
+c = a;
+a = b;
+b = c;
+
+// treatment 
+
+document.getElementById('ap');
+document.getElementById('bp');
+
+ap.innerHTML = a;
+bp.innerHTML = b;
