07a8a0a2f733acf1e7078b382e32ad070aa7fd38
redahm <hammada.reda.dev@gmail.com>
2021-12-06 15:39:36 +0100

    do while and for loops added

---
+++ loops/for/script.js
@@ -0,0 +1,14 @@
+ // variable declaration 
+ var name;
+ var paragraph;
+
+ // saiser 
+ name = "reda";
+ paragraph = document.getElementById('para')
+ // treatment 
+ for(i = 1; i <= 5; i++){
+
+    document.write(name + "<br>");
+
+ }
+
