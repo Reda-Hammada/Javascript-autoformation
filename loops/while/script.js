fc956c894a18da8e6dd9d0dceb4236d3fb15e617
redahm <hammada.reda.dev@gmail.com>
2021-12-06 13:45:24 +0100

    while example added

---
+++ loops/while/script.js
@@ -0,0 +1,13 @@
+
+// variables declaration 
+var paragraph;
+var number;
+// saiser:
+paragraph = document.getElementById('pr');
+number = 0;
+
+// treatment 
+while(number <= 5){
+    number++;
+    paragraph.innerHTML = "true";
+}
