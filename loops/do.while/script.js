3414e3369377d6d8faf459cdcfa6f8b591b085cb
redahm <hammada.reda.dev@gmail.com>
2021-12-08 10:31:56 +0100

    update

---
+++ loops/do.while/script.js
@@ -0,0 +1,17 @@
+// variables declaration 
+var number;
+var paragraph;
+
+// saiser 
+number = 6;
+paragraph = document.getElementById('para');
+
+//traitment 
+
+do{
+    
+    paragraph.innerHTML = "false";
+    number++;
+
+}
+while(number == 3);
