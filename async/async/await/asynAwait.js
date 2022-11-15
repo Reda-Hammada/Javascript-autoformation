7095316deec131eb3690dabe03c73ee2e360fac1
Reda Hammada <hammada.reda.dev@gmail.com>
2022-11-15 17:52:20 +0100

    update async await added

---
+++ async/async/await/asynAwait.js
@@ -0,0 +1,25 @@
+// async function 
+async function greet (){
+
+    return 'Hello';
+}
+console.log(greet());
+
+// await with async function 
+
+  async function greetInGerman(){
+     
+    let promise  = new Promise((resolve, reject)=>{
+        
+            setTimeout(()=> resolve('Hallo, wie geht es dir'),2000);
+
+    })
+
+    let result = await promise;
+
+    console.log(result);
+
+
+}
+
+greetInGerman();
