0e1c163da486bc761181034ec3aaa35132196889
Reda Hammada <hammada.reda.dev@gmail.com>
2023-01-16 16:08:39 +0100

    lexical environment

--- execution_context/exe_context.js
+++ execution_context/exe_context.js
@@ -12,7 +12,7 @@ function sayMyName() {
 
 
 // create an execution context 
-sayMyName();
+window.sayMyName();
 
 /**
  * when javascript code run it creates a global execution context and when a function is run it starts an execution context
@@ -25,3 +25,10 @@ sayMyName();
 // global object and this keyword
 var a = 'Reda';
 console.log(window)
+
+/*lexical environment and analysis 
+  is where you write something,
+  simply where the words are written
+  and their location 
+*/
+
