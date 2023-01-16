0459f8e5597542fd10dd636a8b613bddbeb10338
Reda Hammada <hammada.reda.dev@gmail.com>
2023-01-16 15:38:04 +0100

    execution context

---
+++ execution_context/exe_context.js
@@ -0,0 +1,27 @@
+function printMyName() {
+  console.log( "Reda Hammada");
+}
+
+function findName() {
+  return printMyName();
+}
+
+function sayMyName() {
+  return findName();
+}
+
+
+// create an execution context 
+sayMyName();
+
+/**
+ * when javascript code run it creates a global execution context and when a function is run it starts an execution context
+ * prinMyName()
+ * findName()
+ * sayMyname()
+ * global()
+ */
+
+// global object and this keyword
+var a = 'Reda';
+console.log(window)
