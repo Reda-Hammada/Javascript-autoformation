34f13bdc95a9d443795a1076d5f619aa7a46f3e2
Reda Hammada <hammada.reda.dev@gmail.com>
2023-02-14 17:30:02 +0100

    update this keyword

--- this/this.js
+++ this/this.js
@@ -1,14 +1,37 @@
-// this is the object that the function is a property of 
-// this refers to the object that the function is a property 
-
+// this is the object that the function is a property of
+// this refers to the object that the function is a property
 const person = {
-    name:'Reda',
-    sayMyName(){
+  name: "Reda",
+  sayMyName() {
+    console.log(`this is ${this.name}`);
+  },
+};
 
-        console.log(`this is ${this.name}`)
-    },
+// global object
 
-}
+/*this will not work because the function is not registered in the global object
+ because this outside of the method or the function refers to the global window */
 
+// this.sayMyName();
 
-this.sayMyName();
+/* arrow function  are lexically bound without the arrow function
+   the anotherFunc would call for the window object 
+   instead of the obj object
+*/
+const obj = {
+  name: "Billy",
+  sing() {
+    console.log("a", this);
+    const anotherFunc1 = () => {
+      console.log("b", this);
+    };
+    anotherFunc1();
+    // another alternative is to bind the function to the object
+     function anotherFunc2() {
+      console.log("c", this);
+    }
+     anotherFunc2.bind(this)
+     anotherFunc2()
+  },
+};
+obj.sing();
