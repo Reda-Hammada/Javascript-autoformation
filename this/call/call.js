2ec2082d347ade6670dc04011d5a6b3d53847d49
Reda Hammada <hammada.reda.dev@gmail.com>
2023-02-23 01:01:38 +0100

    more example use of apply and call

--- this/call/call.js
+++ this/call/call.js
@@ -3,3 +3,27 @@ function a(){
 }
 // call invoke a function so a(); is a shorthand for a.call()
 a.call();
+
+
+const wizard = {
+    name:'Merlin',
+    health: 50,
+    heal(){
+        return this.health = 100;
+    }
+}
+
+const archer = {
+    name:'Robin Hood',
+    health: 30,
+
+}
+
+console.log('1',archer);
+wizard.heal.call(archer);
+console.log('2',archer)
+
+/**
+ * for apply it is the same thing the only difference between 
+   it and call is apply take an array when it comes to multiple parameters
+ */
