c81430e03b48babc02f2c5a2b5703236b676ae53
Reda Hammada <hammada.reda.dev@gmail.com>
2023-03-03 05:46:36 +0100

    update

--- type/jsType.js
+++ type/jsType.js
@@ -20,3 +20,13 @@ a.hi = 'hello';
 console.log(a.hi);
 
 
+var array1= [1,2,3];
+var array2 = {
+    0:'1',
+    1:'2',
+    2:'3'
+}
+
+
+console.log(Array.isArray(array1))
+console.log(Array.isArray(array2))
