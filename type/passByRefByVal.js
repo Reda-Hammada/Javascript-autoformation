086adcbebafd5cf5f4f528d0b2ffbc14a4e3767d
Reda Hammada <hammada.reda.dev@gmail.com>
2023-03-10 06:24:51 +0100

    update pass by value and pass by reference

---
+++ type/passByRefByVal.js
@@ -0,0 +1,22 @@
+// pass by reference
+let obj1 = {
+    name:'Reda',
+    password:123,
+} 
+
+let obj2 = obj1
+obj2.password ='easypeasylemonsquizy'
+
+console.log(obj1);
+console.log(obj2);
+
+
+
+
+// pass by value
+// primite types are passed by values 
+let a = 5;
+let b = a;
+b++
+console.log(a);
+console.log(b); 
